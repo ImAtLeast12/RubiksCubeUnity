fa59896ccce3724c66038a6f5ccb0c15a1fd19fe
ImAtLeast12 <imatleast12@gmail.com>
2017-10-27 11:41:10 -0400

    Just Planes Initial Commit

--- MakeCube.cs
+++ MakeCube.cs
@@ -2,143 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MakeCube : MonoBehaviour {
-
-	public GameObject cubelet;
-
-	int[,] cube = new int[,] { 	{ 	0,0,0,
-									0,0,0,
-									0,0,0, }, 
-		
-								{ 	1,1,1,
-									1,1,1,
-									1,1,1, }, 
-		
-								{ 	2,2,2,
-									2,2,2,
-									2,2,2, }, 
-		
-								{ 	3,3,3,
-									3,3,3,
-									3,3,3, },
-		
-								{ 	4,4,4,
-									4,4,4,
-									4,4,4, },
-		
-								{ 	5,5,5,
-									5,5,5,
-									5,5,5, }};
+public class createCube : MonoBehaviour {
+	
 
 
-	string printCube(){
-		string total = "";
-		for (int i = 0; i < 3; i++) 
-			total += System.String.Format("\t{0},{1},{2},\n", cube[0,3*i], cube[0,1+ (3*i)], cube[0,+ (3*i)]);
-		total += "\n";
-		for (int i = 0; i < 3; i++) 
-			total += System.String.Format ("{0},{1},{2},    {3},{4},{5},    {6},{7},{8},    {9},{10},{11},\n", cube [1, 0 + (3 * i)], cube [1, 1 + (3 * i)], cube [1, 2 + (3 * i)], cube [2, 0 + (3 * i)], cube [2, 1 + (3 * i)], cube [2, 2 + (3 * i)], cube [3, 0 + (3 * i)], cube [3, 1 + (3 * i)], cube [3, 2 + (3 * i)], cube [4, 0 + (3 * i)], cube [4, 1 + (3 * i)], cube [4, 2 + (3 * i)]);
-		total += "\n";
-		for (int i = 0; i < 3; i++) 
-			total += System.String.Format("\t{0},{1},{2},\n", cube[5,0+ (3*i)], cube[5,1+ (3*i)], cube[5,2+ (3*i)]);
-		
-		return total;
-	}
+	public GameObject[] face;
+	public Material [] colors;
+	private Renderer rend;
+
+	private int[,] cube = {
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, },
+		{ 2, 2, 2, 2, 2, 2, 2, 2, 2, },
+		{ 3, 3, 3, 3, 3, 3, 3, 3, 3, },
+		{ 4, 4, 4, 4, 4, 4, 4, 4, 4, },
+		{ 5, 5, 5, 5, 5, 5, 5, 5, 5, }};
+
+	private float[,] transformations={
+		{0,1.5f,0	,0,0,0,0},		
+		{2,-1.5f,0	,0,0,180,0},	
+		{-.5f,-1,0	,0,0,90,90},		
+
+		{2.5f,1,0	,0,0,-90,90},
+		{0,1,2.5f		,90,0,0,90},
+		{0,-1,-.5f		,90,0,0,-90}
+	}; 
 
 	void Start () {
 
-		Debug.Log(printCube ());
+		GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		plane.gameObject.transform.localScale = new Vector3(.1f,.1f,.1f);
 
-		for (int x = 0; x < 3; x++) {
-			for (int y = 0; y < 3; y++){
-				for (int z = 0; z < 3; z++){
-					Instantiate (cubelet, new Vector3 (x, y, z), new Quaternion (0, 0, 0, 0));
+		for (int side = 0; side < 6; side++) {
+			face [side] = Instantiate (new GameObject(), new Vector3 (0, 0, 0), new Quaternion (0, 0, 0, 0));
+			face [side].name = "Face " + side.ToString ();
+			face [side].transform.parent = this.transform;
+			for (int x = 0; x < 3; x++) { 
+				for (int y = 0; y < 3; y++) {
+					var cubeletFace = Instantiate (plane, new Vector3 (x, 0, y), new Quaternion (0, 0, 0, 0));	
+					rend = cubeletFace.GetComponent<MeshRenderer> ();
+					rend.material = colors [cube [side, x * 3 + y]];
+					cubeletFace.name = "Plane " + side.ToString () + x.ToString () + y.ToString ();
+					cubeletFace.transform.parent = face [side].transform;
 				}
 			}
+			face [side].transform.position = new Vector3 (transformations [side, 0], transformations [side, 1], transformations [side, 2]);
+			face[side].transform.rotation = new Quaternion(transformations[side,3],transformations[side,4],transformations[side,5],transformations[side,6]);
 		}
 	}
-
-
-
-	/*
-
-		Map:
-
-			B
-		W	R	Y	O
-			G
-			
-			U
-		L	F	R	B
-			D
-
-			0
-		1	2	3	4
-			5
-
-
-		Specifically
-
-		Corners:		Should have 4*2 = 	8
-		Edges: 			Should have 4*3 = 	12
-		Centers:		Should have 		6
-
-
-
-		More Detailed Map:
-		
-    				  0	0,1,2,
-	    				3,4,5,
-		    		 	6,7,8,
-
-		  1	0,1,2,	  2	0,1,2,	  3	0,1,2,	  4	0,1,2
-			3,4,5,		3,4,5,		3,4,5,		3,4,5
-			6,7,8,		6,7,8,		6,7,8,		6,7,8
-
-					  5	0,1,2,
-						3,4,5,
-						6,7,8,
-		Going to use as a Template for the Cube Print Out
-
-		Corners:
-		Cube[0][0]	Cube[1][0]	Cube[4][2]
-		Cube[0][2]	Cube[3][2]	Cube[4][0]	
-		Cube[0][6]	Cube[1][2]	Cube[2][0]	
-		Cube[0][8]	Cube[2][2]	Cube[3][0]	
-		Cube[1][6]	Cube[4][8]	Cube[5][6]
-		Cube[1][8]	Cube[2][6]	Cube[5][0]	
-		Cube[2][8]	Cube[3][6]	Cube[5][2]
-		Cube[3][8]	Cube[4][6]	Cube[5][8]
-
-		Edges:	Might want to sort these later
-			Cube[0][1]	Cube[4][1] (Not Sure)
-			Cube[0][3]	Cube[1][1]
-			Cube[0][5]	Cube[3][1]
-			Cube[0][7]	Cube[2][1]
-			Cube[1][3]	Cube[4][5]
-			Cube[1][5]	Cube[2][3]
-			Cube[1][7]	Cube[5][3]
-			Cube[3][3]	Cube[2][5]
-			Cube[3][5]	Cube[4][3]	
-			Cube[3][7]	Cube[5][5]
-			Cube[4][7]	Cube[5][7]  (Not Sure)
-			Cube[5][1]	Cube[2][7]  
-
-		Centers:
-			cube[0][4]
-			cube[1][4]
-			cube[2][4]
-			cube[3][4]
-			cube[4][4]
-			cube[5][4]
-
-		Observatios:
-		4's are center points
-
-		Odd Numbers are Edges
-
-		Even Numbers are Corners
-		
-	*/
-
 }
